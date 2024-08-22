df27fdce4ca1759470c367d9c81686e6f610a238
danielafamily <ddanielafamily@gmail.com>
2024-08-22 16:21:09 +0200

    Revert "adding feature generate pdf"
    This reverts commit c0b306d73cf3a6ab137dee7570b73f37e1a76f72.

--- app/app.js
+++ app/app.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
         'avengers-endgame-batalla-final.jpg', 'marvel-studios-logo.jpg',
         'avengers-endgame.jpg', 'marvel-studios-logo.jpg',
         'nebula-del-espacio.jpg', 'tierra-y-luna-a-la-luz-del-sol.jpg',
-        'pelicula-guardianes-de-la-galaxia.jpg', 'espacio-estrellas-universo-nebulosa.jpg'
-        // Add more card filenames here if needed
+        'pelicula-guardianes-de-la-galaxia.jpg', 'espacio-estrellas-universo-nebulosa.jpg' 
+        // Add more card filenames here
     ];
 
     let originalDeck = [...cards]; // Copy of the original deck to reset later
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     drawCardBtn.addEventListener('click', () => {
         if (cards.length === 0) {
             alert('El Cartón está lleno!!!');
-            generatePDF(); // Generate PDF when all cards are drawn
             return;
         }
 
@@ -38,12 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
         imgElement.src = `assets/images/${drawnCard}`;
         imgElement.alt = drawnCard;
         drawnCardsContainer.appendChild(imgElement);
-
-        // Check if 16 cards have been drawn, then generate PDF
-        if (drawnCards.length === 16) {
-            alert('El Cartón está lleno!!!');
-            generatePDF(); // Generate PDF when all cards are drawn
-        }
     });
 
     resetGameBtn.addEventListener('click', () => {
@@ -63,38 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
         alert('El cartón será reiniciado!!!');
     });
-
-    function generatePDF() {
-        const { jsPDF } = window.jspdf;
-        const doc = new jsPDF();
-
-        // Add title
-        doc.setFontSize(18);
-        doc.text('Cartón de Lotería', 105, 20, null, null, 'center');
-
-        // Positioning variables
-        const startX = 20;
-        const startY = 30;
-        const cardWidth = 40;
-        const cardHeight = 60;
-        let x = startX;
-        let y = startY;
-
-        // Loop through the drawn cards and add them to the PDF
-        drawnCards.forEach((card, index) => {
-            doc.addImage(`assets/images/${card}`, 'JPEG', x, y, cardWidth, cardHeight);
-
-            // Move to the next column
-            x += cardWidth + 10;
-
-            // Move to the next row after 4 columns
-            if ((index + 1) % 4 === 0) {
-                x = startX;
-                y += cardHeight + 10;
-            }
-        });
-
-        // Save the PDF with a specific name
-        doc.save('carton_loteria.pdf');
-    }
 });
